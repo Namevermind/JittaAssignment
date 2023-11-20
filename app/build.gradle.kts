91bd0bd50ef640ce7e17c0a9b77526ff2628cdc6
b.boonpitaksakul <osacn_b.boonpitaksakul@krungthai.com>
2023-11-20 15:03:57 +0700

    sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "android.jitta.assignment"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "android.jitta.assignment"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
